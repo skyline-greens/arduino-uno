2e319e1338ac31091bc251f4d4430281a2baf4cb
kheiro-toubal <kheiro1593@gmail.com>
2025-03-21 20:11:33 +0100

    Removed the actuators from arduino and moved them to the esp

--- arduino-code.ino
+++ arduino-code.ino
@@ -4,10 +4,6 @@
 #define DHTPIN 2
 #define DHTTYPE DHT11
 
-#define AIR_PUMP_PIN 3  
-#define WATER_PUMP_PIN 4  
-#define LIGHT_PIN 5  
-
 #define LDR_PIN A0      
 #define CO2_PIN A1      
 #define SOIL_PIN A2      
@@ -18,32 +14,9 @@ DHT dht(DHTPIN, DHTTYPE);
 void setup() {
     Serial.begin(115200);
     dht.begin();
-
-    pinMode(AIR_PUMP_PIN, OUTPUT);
-    pinMode(WATER_PUMP_PIN, OUTPUT);
-    pinMode(LIGHT_PIN, OUTPUT);
 }
 
 void loop() {
-    if (Serial.available()) {  
-        String command = Serial.readStringUntil('\n');
-        command.trim();
-        
-        if (command == "AIR_PUMP ON") {
-            digitalWrite(AIR_PUMP_PIN, HIGH);
-        } else if (command == "AIR_PUMP OFF") {
-            digitalWrite(AIR_PUMP_PIN, LOW);
-        } else if (command == "WATER_PUMP ON") {
-            digitalWrite(WATER_PUMP_PIN, HIGH);
-        } else if (command == "WATER_PUMP OFF") {
-            digitalWrite(WATER_PUMP_PIN, LOW);
-        } else if (command == "LIGHT ON") {
-            digitalWrite(LIGHT_PIN, HIGH);
-        } else if (command == "LIGHT OFF") {
-            digitalWrite(LIGHT_PIN, LOW);
-        }
-    }
-
     // Read sensors every 2 seconds
     static unsigned long lastReadTime = 0;
     if (millis() - lastReadTime >= 2000) {
